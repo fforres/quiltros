b50f231d74e58d0e4e1bd80526498626c26e2d3a
fforres <felipe.torressepulveda@gmail.com>
2019-04-15 23:03:27 -0700

    Update

---
+++ server.ts
@@ -0,0 +1,33 @@
+import { parse } from "url";
+import match from "micro-route/match";
+import next from "next";
+import { IncomingMessage, ServerResponse } from "http";
+
+const dev = process.env.NODE_ENV !== "production";
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const isA = (req: IncomingMessage) => match(req, "/a");
+const isB = (req: IncomingMessage) => match(req, "/b");
+
+async function main(req: IncomingMessage, res: ServerResponse) {
+  const { url = "" } = req;
+  const parsedUrl = parse(url, true);
+  const { query } = parsedUrl;
+
+  if (isA(req)) {
+    return app.render(req, res, "/b", query);
+  } else if (isB(req)) {
+    return app.render(req, res, "/a", query);
+  }
+
+  return handle(req, res, parsedUrl);
+}
+
+async function setup(handler) {
+  await app.prepare();
+  return handler;
+}
+
+module.exports = setup(main);
