9ae245f494bc57b8a1949a30402d01ecfc1c61f4
fforres <felipe.torressepulveda@gmail.com>
2019-04-15 23:07:14 -0700

    Update

--- server.ts
+++ server.ts
@@ -1,26 +1,26 @@
-import { parse } from "url";
-import match from "micro-route/match";
-import next from "next";
+import match from 'micro-route/match';
+import next from 'next';
+import { parse } from 'url';
 
-import { IncomingMessage, ServerResponse } from "http";
+import { IncomingMessage, ServerResponse } from 'http';
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const isA = (req: IncomingMessage) => match(req, "/a");
-const isB = (req: IncomingMessage) => match(req, "/b");
+const isA = (req: IncomingMessage) => match(req, '/a');
+const isB = (req: IncomingMessage) => match(req, '/b');
 
 async function main(req: IncomingMessage, res: ServerResponse) {
-  const { url = "" } = req;
+  const { url = '' } = req;
   const parsedUrl = parse(url, true);
   const { query } = parsedUrl;
 
   if (isA(req)) {
-    return app.render(req, res, "/b", query);
+    return app.render(req, res, '/b', query);
   } else if (isB(req)) {
-    return app.render(req, res, "/a", query);
+    return app.render(req, res, '/a', query);
   }
 
   return handle(req, res, parsedUrl);
