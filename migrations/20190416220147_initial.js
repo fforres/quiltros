e12ebca6d18d5e866953958d254800afef632095
fforres <felipe.torressepulveda@gmail.com>
2019-04-16 23:20:14 -0700

    update

--- migrations/20190416220147_initial.js
+++ migrations/20190416220147_initial.js
@@ -1,15 +1,20 @@
-const { NODE_ENV } = process.env
+const { NODE_ENV } = process.env;
 
 exports.up = async (knex, Promise) => {
-  NODE_ENV && await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+  NODE_ENV && (await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'));
   await knex.schema.createTable('users', table => {
-    NODE_ENV ? table.uuid('hash').primary().defaultTo(knex.raw('uuid_generate_v4()')) : table.uuid('hash').primary()
+    NODE_ENV
+      ? table
+          .uuid('hash')
+          .primary()
+          .defaultTo(knex.raw('uuid_generate_v4()'))
+      : table.uuid('hash').primary();
     table.string('first_name');
     table.string('last_name');
-  })
+  });
 };
 
 exports.down = async (knex, Promise) => {
-  NODE_ENV && await knex.raw('drop extension if exists "uuid-ossp"')
-  await knex.schema.dropTable('users')
+  NODE_ENV && (await knex.raw('drop extension if exists "uuid-ossp"'));
+  await knex.schema.dropTable('users');
 };
