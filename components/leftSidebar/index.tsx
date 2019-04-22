b4653568c748a61b781903999c3a31a040194607
fforres <felipe.torressepulveda@gmail.com>
2019-04-21 22:06:26 -0700

    datos mascota 101

--- components/leftSidebar/index.tsx
+++ components/leftSidebar/index.tsx
@@ -1,8 +1,22 @@
 /** @jsx jsx */
-import { Card, Elevation } from '@blueprintjs/core';
+import {
+  Card,
+  Elevation,
+  FormGroup,
+  H5,
+  InputGroup,
+  Label,
+  NumericInput,
+  Pre
+} from '@blueprintjs/core';
 import { jsx } from '@emotion/core';
 import React from 'react';
-import { sidebarContainerStyle } from './style';
+import {
+  ageButtonWrapper,
+  buttonLeftStyle,
+  buttonRightStyle,
+  sidebarContainerStyle
+} from './style';
 
 export interface ILeftSidebarProps {}
 
@@ -13,8 +27,27 @@ export default class LeftSidebar extends React.Component<
   render() {
     return (
       <Card elevation={Elevation.ONE} css={sidebarContainerStyle}>
-        <h1>asdads</h1>
-        <p>qwe</p>
+        <div>
+          <FormGroup label='Nombre'>
+            <InputGroup placeholder='Nombre' intent='primary' />
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup label='Edad'>
+            <div css={ageButtonWrapper}>
+              <InputGroup
+                placeholder='Años'
+                intent='primary'
+                css={buttonLeftStyle}
+              />
+              <InputGroup
+                placeholder='Meses'
+                intent='primary'
+                css={buttonRightStyle}
+              />
+            </div>
+          </FormGroup>
+        </div>
       </Card>
     );
   }
--- components/leftSidebar/style.ts
+++ components/leftSidebar/style.ts
@@ -2,5 +2,17 @@ import { css } from '@emotion/core';
 
 export const sidebarContainerStyle = css`
   margin: 1rem;
-  width: 250px;
+  min-width: 300px;
+  width: 300px;
+`;
+
+export const ageButtonWrapper = css`
+  display: inline-flex;
+`;
+export const buttonLeftStyle = css`
+  padding-right: 0.5rem;
+`;
+
+export const buttonRightStyle = css`
+  padding-left: 0.5rem;
 `;
