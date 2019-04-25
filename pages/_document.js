08d6ac97932eedd860379b3aa19fd384fd3141dd
Felipe Torres (fforres) <FELIPE.TORRESSEPULVEDA@GMAIL.COM>
2019-04-24 23:24:29 -0700

    ADDING GA (#11)
    * ADDING GA
    
    * whoops client side

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,16 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { resetCss } from './resetcss';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-139082777-1', { debug: true, gaOptions: {
+  siteSpeedSampleRate: true,
+  alwaysSendReferrer: true,
+  cookieName: '_quiltros_ga'
+}})
 
 class MyDocument extends Document {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
