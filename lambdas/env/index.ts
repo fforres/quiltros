dddc78e32deb5fe0148bd6ab3e425c07fb27bd42
fforres <felipe.torressepulveda@gmail.com>
2019-04-28 11:58:25 -0700

    update

---
+++ lambdas/env/index.ts
@@ -0,0 +1,9 @@
+import { IncomingMessage, ServerResponse } from "http"
+
+export default async (_: IncomingMessage, res: ServerResponse) => {
+
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify(process.env),
+  );
+};
