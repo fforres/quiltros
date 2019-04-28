70389ffb53c5d03beae14749de06320075a7f018
fforres <felipe.torressepulveda@gmail.com>
2019-04-28 12:57:28 -0700

    update\

--- lambdas/image/index.ts
+++ lambdas/image/index.ts
@@ -1,21 +1,10 @@
 import { IncomingMessage, ServerResponse } from 'http'
-import aws from 'aws-sdk'
-
-import { accessKeyId, secretAccessKey } from '../env'
+import { s3Client } from '../s3';
 import { lambdaJsonResponseHandler } from '../utils';
-
-aws.config.region = 'us-west-2'
-aws.config.setPromisesDependency(Promise)
-
-export const s3Client = new aws.S3({
-  credentials: {
-    accessKeyId,
-    secretAccessKey,
-  }
-})
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
   lambdaJsonResponseHandler(req, res, async () => {
+    debugger
     return {}
   })
 };
