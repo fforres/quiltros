21c4bac83444fc400cf2228dd2f78435fec638d1
fforres <felipe.torressepulveda@gmail.com>
2019-04-29 18:04:59 -0700

    separate handleImageUploadForm

--- lambdas/image/index.ts
+++ lambdas/image/index.ts
@@ -5,8 +5,8 @@ import { inspect } from 'util';
 import { s3Client } from '../s3';
 import { lambdaJsonResponseHandler } from '../utils';
 
-const promiseParser = (req): Promise<{ fields: any; files: any }> =>
-  new Promise((resolve, reject) => {
+const promiseParser = (req): Promise<{ fields: any; files: any }> => {
+  return new Promise((resolve, reject) => {
     const form = new formidable.IncomingForm();
     form.parse(req, (err, fields, files) => {
       if (err) {
@@ -15,29 +15,34 @@ const promiseParser = (req): Promise<{ fields: any; files: any }> =>
       resolve({ fields, files });
     });
   });
-export default lambdaJsonResponseHandler(
-  async (req: IncomingMessage, res: ServerResponse) => {
-    const { fields, files } = await promiseParser(req);
-    console.log(fields);
-    console.log(files);
-    // s3Client.upload
+};
 
-    // //get "body" args from header
-    // const { id, fn } = JSON.parse(req.get('body'));
-    // const Key = id + '/' + fn; //upload to s3 folder "id" with filename === fn
-    // const params = {
-    //   Key,
-    //   Bucket: bucketName, //set somewhere
-    //   Body: req, //req is a stream
-    // };
-    // s3.upload(params, (err, data) => {
-    //   if (err) {
-    //     res.send('Error Uploading Data: ' + JSON.stringify(err) + '\n' + JSON.stringify(err.stack));
-    //   } else {
-    //     res.send(Key);
-    //   }
-    // });
+export const handleImageUploadForm = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  const { fields, files } = await promiseParser(req);
+  console.log(fields);
+  console.log(files);
+  // s3Client.upload
 
-    return {};
-  }
-);
+  // //get "body" args from header
+  // const { id, fn } = JSON.parse(req.get('body'));
+  // const Key = id + '/' + fn; //upload to s3 folder "id" with filename === fn
+  // const params = {
+  //   Key,
+  //   Bucket: bucketName, //set somewhere
+  //   Body: req, //req is a stream
+  // };
+  // s3.upload(params, (err, data) => {
+  //   if (err) {
+  //     res.send('Error Uploading Data: ' + JSON.stringify(err) + '\n' + JSON.stringify(err.stack));
+  //   } else {
+  //     res.send(Key);
+  //   }
+  // });
+
+  return {};
+};
+
+export default lambdaJsonResponseHandler(handleImageUploadForm);
