865a277dbb5fb2ebde85a089f5dfa983f1970f35
fforres <felipe.torressepulveda@gmail.com>
2019-05-05 14:10:33 -0700

    update

--- client/lib/error_handling.ts
+++ client/lib/error_handling.ts
@@ -1,4 +1,4 @@
-import Sentry from '@sentry/node';
+import * as Sentry from '@sentry/browser';
 
 Sentry.init({
   attachStacktrace: true,
--- lambdas/endpoints/error/index.ts
+++ lambdas/endpoints/error/index.ts
@@ -1,12 +1,8 @@
 import { IncomingMessage, ServerResponse } from 'http';
-// import { captureError, initSentry } from '../../error_handler';
 import { lambdaJsonResponseHandler } from '../../utils';
 
 const envErrorHandler = async (req: IncomingMessage, res: ServerResponse) => {
-  // initSentry('asd');
-  const a = new Error('Dummy error!!');
-  // captureError(a);
-  throw a;
+  throw new Error('Dummy error!!');
 };
 
 export default lambdaJsonResponseHandler(envErrorHandler);
