2af9b8df5a7705bb5394f3f79c8c34e4076bfb08
fforres <felipe.torressepulveda@gmail.com>
2019-05-05 12:59:52 -0700

    update

--- lambdas/endpoints/env/index.ts
+++ lambdas/endpoints/env/index.ts
@@ -2,7 +2,8 @@ import { IncomingMessage, ServerResponse } from 'http';
 import { lambdaJsonResponseHandler } from '../../utils';
 
 const envLambdaHandler = async (req: IncomingMessage, res: ServerResponse) => {
-  return {};
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ hello: ':)' }));
 };
 
-export default lambdaJsonResponseHandler(envLambdaHandler);
+export default envLambdaHandler;
