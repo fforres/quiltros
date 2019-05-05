24f22093033d8b1706f8afaf079a44b7248e2f98
fforres <felipe.torressepulveda@gmail.com>
2019-05-05 13:59:12 -0700

    Update

--- lambdas/error_handler.ts
+++ lambdas/error_handler.ts
@@ -1,4 +1,4 @@
-import Sentry from '@sentry/node';
+import * as Sentry from '@sentry/node';
 
 export const initSentry = (lambdaName: string) => {
   Sentry.configureScope(scope => {
--- lambdas/utils.ts
+++ lambdas/utils.ts
@@ -1,19 +1,20 @@
 import { IncomingMessage, ServerResponse } from 'http';
-// import { captureError, initSentry } from './error_handler';
+import { captureError, initSentry } from './error_handler';
 
 export const lambdaJsonResponseHandler = (
   cb: (req: IncomingMessage, res: ServerResponse) => Promise<object>
 ) => {
-  // initSentry(cb.name); // Sets the lambda name
+  initSentry(cb.name); // Sets the lambda name
   return async (req: IncomingMessage, res: ServerResponse) => {
     let code = 200;
     let response = {};
     try {
       response = await cb(req, res);
-    } catch (e) {
+    } catch (error) {
+      const e: Error = error;
       code = 500;
-      response = { msg: 'ERROR', errorMessage: e.msg };
-      // await captureError(e);
+      response = { msg: 'ERROR', error_message: e.message };
+      await captureError(e);
       console.error(e);
     }
     res.writeHead(code, { 'Content-Type': 'application/json' });
